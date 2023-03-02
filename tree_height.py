7fdc99b0cb275cce22da9aed909896e9b3c24066
SandijsKrumins1 <sandijs.krumins@edu.rtu.lv>
2023-03-02 23:49:13 +0200

    modified:   tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,8 +2,6 @@
 
 import sys
 import threading
-import numpy
-
 
 class Node:
     def __init__(self, data):
@@ -13,7 +11,6 @@ class Node:
     def add_child(self, child):
         self.children.append(child)
         
-
 def compute_height(node):
     # Write this function
     max_height = 0
@@ -21,10 +18,13 @@ def compute_height(node):
         max_height = max(max_height, compute_height(child))
     return max_height + 1
 
-def create_tree(n):
+def create_tree(n, mode, file=None):
     parent_index=[]
     nodes={}
-    values = input().split()
+    if "I" in mode:
+        values = input().split()
+    else:
+        values = file.readline().split()
     for i in range(n):
         parent_index.append(int(values[i]))
         nodes[i] = Node(i)
@@ -37,24 +37,6 @@ def create_tree(n):
             root = nodes[i]
 
     return root
-
-def create_tree_file(n, file):
-    parent_index=[]
-    nodes={}
-    values = file.readline().split()
-    for i in range(n):
-        parent_index.append(int(values[i]))
-        nodes[i] = Node(i)
-          
-    for i in range(n):
-        if parent_index[i] != -1:
-            nodes[parent_index[i]].add_child(nodes[i])
-        
-        else:
-            root = nodes[i]
-
-    return root
-
 
 def main():
     print("Input Mode: ")
@@ -62,7 +44,7 @@ def main():
     if "I" in mode:
         print("Input: ")
         n = int(input())
-        root = create_tree(n)
+        root = create_tree(n, mode)
         height = compute_height(root)
         print(height)
 
@@ -75,23 +57,10 @@ def main():
         folder = './test/'
         file = open(folder + filename, 'r')
         n = int(file.readline())
-        root = create_tree_file(n, file)
+        root = create_tree(n, mode, file)
         height = compute_height(root)
         print(height)
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
+        
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-# print(numpy.array([1,2,3]))
